3ebd87c90c0744cadbf4f04fc99a22db31ec228e
Tony Menza <ajm043@bucknell.edu>
2022-01-26 19:24:39 -0500

    completed project 1 birthday reminder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  // Setting people to data which is an array of objects
+  const [people, setPeople] = useState(data);
+  return (
+    
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        {/* passing people array as prop in our List component */}
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
