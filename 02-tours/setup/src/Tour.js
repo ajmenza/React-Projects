70863ebd8a3d59bb400fcbe6eab6fe9b4666a727
Tony Menza <ajm043@bucknell.edu>
2022-02-02 21:56:54 -0500

    completed tours project

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
-
-const Tour = () => {
-  return <h2>tour component</h2>;
+import React, { useState } from "react";
+// destructuring props previously named in Tours component (with spread operator) in Tours.js
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        {/* substring() tells how much of the string to show */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+// import tours prop we named in <Tours> component in App.js
+const Tours = ({tours, removeTour}) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          // use spread operator to set each key from tour object to a prop
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
