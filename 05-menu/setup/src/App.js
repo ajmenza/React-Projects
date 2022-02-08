497fdaaca6af27526a09b00d94cdc87b7802995d
Tony Menza <ajm043@bucknell.edu>
2022-02-07 19:55:28 -0500

    completed categories project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,34 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// Sets only gets unique values so everytime we loop over an item we won't repeat the item's category in the allCategories array. We use the spread operator to destructure the set so we have an array now rather than a Set object
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    // User chooses a category which changes the state and then filters by checking if the items in the current list match the chosen category
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  } 
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems}/>
+      <Menu items={menuItems}/>
+    </section>
+  </main>
 }
 
 export default App;
