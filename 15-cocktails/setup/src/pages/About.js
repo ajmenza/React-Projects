7e80a45a5bcafb069e6577f7bd57515c85750733
Tony Menza <ajm043@bucknell.edu>
2022-03-02 17:34:57 -0500

    completed cocktails project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, at explicabo velit amet animi voluptates obcaecati. Molestiae totam sequi porro, dolores consequatur, accusamus voluptatum cum labore exercitationem iure dolorem officia nemo quisquam blanditiis placeat. Impedit ducimus esse velit! Est, sequi.</p>
+    </section>
   )
 }
 
